c4a110ceb0a31369a129ccabbe76d4a3156056dd
Persona-ity <studyscience1955@gmail.com>
2024-01-26 01:41:56 +0700

    Добавление компонента col1

--- js/main.js
+++ js/main.js
@@ -44,3 +44,69 @@ Vue.component('Kanban', {
 
     }
 })
+
+Vue.component('col1', { 
+   template: `
+       <div class="col">
+           <h2>Запланированные задачи</h2>
+           <li class="cards" v-for="card in column1">
+               <a @click="deleteCard(card)">Удалить</a> | <a @click="card.editB = true">Редактировать</a> <br>
+               <p class="card-title">{{card.title}}</p>
+               <ul>
+                   <li class="tasks">Описание: {{card.description}}</li>
+                   <li class="tasks">Дата создания:
+                   {{ card.date }}</li>
+                   <li class="tasks">Дедлайн: {{card.deadline}}</li>
+                   <li class="tasks" v-if="card.edit != null">Последнее изменение: {{ card.edit}}</li>
+                   <li class="tasks" v-if="card.editB">
+                       <form @submit.prevent="updateTask(card)">
+                           <p>Новый заголовок: 
+                               <input type="text" v-model="card.title" maxlength="30" placeholder="Заголовок">
+                           </p>
+                           <p>Новое описание: 
+                               <textarea v-model="card.description" cols="20" rows="5"></textarea>
+                           </p>
+                           <p>
+                               <input type="submit" value="Редактировать">
+                           </p>
+                       </form>
+                   </li>
+               </ul>
+               <a @click="nextcol(card)">Следующая колонка</a>
+           </li >
+       </div>
+   `,
+   props: {
+       column1: {
+           type: Array,
+       },
+       column2: {
+           type: Array,
+       },
+       card: {
+           type: Object
+       },
+       errors: {
+           type: Array
+       }
+   },
+   methods: {
+       nextcol(card) {
+           this.column1.splice(this.column1.indexOf(card), 1)
+           eventBus.$emit('addColumn2', card)
+       },
+       deleteCard(card) {
+           this.column1.splice(this.column1.indexOf(card), 1)  
+       },
+       updateTask(card){
+           card.editB = false
+           this.column1.push(card)
+           this.column1.splice(this.column1.indexOf(card), 1)
+           card.edit = new Date().toLocaleString()
+       }
+       
+   },
+   computed: {
+
+   },
+})
